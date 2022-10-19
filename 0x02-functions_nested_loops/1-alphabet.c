9d60275db317fdb1a461e0e676e0a87eca11fc2e
AlvinOt <thisisalvinhere@gmail.com>
2022-10-19 05:17:20 +0000

    123

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,14 @@
  * print alphabet - in lower case
  */
 
-void print_alphabet(void);
+int main(void)
 {
 	char alx;
+
 	for (alx = 'a'; alx <= 'z'; alx++)
 		_putchar(alx);
 
 	_putchar('\n');
+
+	return (0);
 }
