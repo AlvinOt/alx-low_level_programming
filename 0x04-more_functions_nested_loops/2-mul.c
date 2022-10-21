6cabe213decce6f49e0e50bb75492ef4db2f2c50
AlvinOt <thisisalvinhere@gmail.com>
2022-10-21 22:30:56 +0000

    twoToFive

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,3 +1,13 @@
 #include "main.h"
 
+/**
+ * mul - multiplies integers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication result
+ */
 
+int mul(int a, int b)
+{
+	return (a * b);
+}
