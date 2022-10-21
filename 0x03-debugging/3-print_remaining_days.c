1647958f5872c51a9aa563b2ea75a69003e6c228
AlvinOt <thisisalvinhere@gmail.com>
2022-10-21 20:24:41 +0000

    krr

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,17 +13,12 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0)
 	{
-		while (month == 2)
-		{
-			day++;
-		}
+		day++;
+	}
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
-	}
 	else
-	{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
-	}
 }
