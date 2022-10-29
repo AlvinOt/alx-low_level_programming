54cdfdaddc629ba41d9977e68fd204fb36010ae5
AlvinOt <thisisalvinhere@gmail.com>
2022-10-29 12:19:17 +0000

    task do3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,12 +4,12 @@
  * _strcmp - compares pointers to two strings
  * @s1: a pointer to first string to compare
  * @s2: a pointer to second string to compare
- * Return: negative difference, or 0 or positive difference
+ * Return: negative difference, or zero, or positive difference
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && s1 == *s2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
