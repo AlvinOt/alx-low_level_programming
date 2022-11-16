aa0ea12b935cd70fd5a9fad7570ff50d47af11a8
AlvinOt <thisisalvinhere@gmail.com>
2022-11-16 02:51:38 +0000

    malloc free dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,55 @@
-:`
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - a new dog
+ * @name: the dog's name
+ * @age: dog's age
+ * @owner:dog's owner
+ * Return: newdog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0, k;
+	dog_t *dawg;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	dawg = malloc(sizeof(dog_t));
+	if (dawg == NULL)
+	{
+		free(dawg);
+		reutrn(NULL);
+	}
+
+	dawg->name = malloc(i * sizeof(dawg->name));
+	if (dawg->name == NULL)
+	{
+		free(dawg->name);
+		free(dawg);
+		return (NULL);
+	}
+
+	for (k = 0; k <= i; k++)
+		dawg->name[k] = name[k];
+	dawg->age = age;
+	dog->owner = malloc(j * sizeof(dawg->owner));
+	if (dawg->owner == NULL)
+	{
+		free(dawg->owner);
+		free(dawg->name);
+		free(dawg);
+		return (NULL);
+	}
+
+	for (k = 0; k <= j; k++)
+		dawg->owner[k] = owner[k];
+
+	return (dawg);
+
+
+}
