2f0931f649e28199bced76ad52fadd982b7ac9ec
AlvinOt <thisisalvinhere@gmail.com>
2023-01-20 13:16:03 +0000

    dynamo lib

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to integer
+ * @s: string converted
+ * Return: integer  value of converted string
+ */
+
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
+
+	return (0);
+}
